beffc586268d207dcb1cfa26fb17822f40631a02
Eduardo <ealvarez@carminaticonsulting.com>
2023-10-02 12:16:15 -0500

    Adding new DB table and migrations

---
+++ BulkyWeb/BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+        public IActionResult Index()
+        {
+            var categoryList = _dbContext.Categories.ToList();
+            return View(categoryList);
+        }
+    }
+}
