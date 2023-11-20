aa11d4373761e0e15e4c643d6a482840b3a614aa
Eduardo <ealvarez@carminaticonsulting.com>
2023-11-20 13:44:02 -0600

    created edit and delete post and get methods

--- BulkyWeb/BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/BulkyWeb/Controllers/CategoryController.cs
@@ -17,10 +17,12 @@ namespace BulkyWeb.Controllers
             return View(categoryList);
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Create(Category category)
         {
@@ -35,6 +37,60 @@ namespace BulkyWeb.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _dbContext.Categories.FirstOrDefault(x => x.CategoryId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _dbContext.Categories.FirstOrDefault(x => x.CategoryId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? category = _dbContext.Categories.FirstOrDefault(x => x.CategoryId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Categories.Remove(category);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
