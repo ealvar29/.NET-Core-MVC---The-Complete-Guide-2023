79cb7bb8ed820a6bd37b8592b2bc4920a2bc21a9
Eduardo <ealvarez@carminaticonsulting.com>
2023-11-21 16:44:45 -0600

    added razor project

---
+++ BulkyWeb/BulkyWebRazor_Temp/Data/ApplicationDBContext.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace BulkyWebRazor_Temp.Data
+{
+        public class ApplicationDBContext : DbContext
+        {
+            public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+            {
+            }
+
+            public DbSet<Category> Razor_Categories { get; set; }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Category>().HasData(
+                    new Category { CategoryId = 1, Name = "Action", DisplayOrder = 1 },
+                    new Category { CategoryId = 2, Name = "Science Fiction", DisplayOrder = 2 },
+                    new Category { CategoryId = 3, Name = "History", DisplayOrder = 3 }
+                    );
+            }
+        }
+}
